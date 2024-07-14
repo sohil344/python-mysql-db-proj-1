17610cb43e4dc6a05e1189b1f1a7a04d93d3ab00
sohil <doshisohil7@gmail.com>
2024-07-14 22:32:29 +0530

    rds block uncommented

--- infra/main.tf
+++ infra/main.tf
@@ -75,7 +75,7 @@ module "aws_ceritification_manager" {
   domain_name    = var.domain_name
   hosted_zone_id = module.hosted_zone.hosted_zone_id
 }
-/*
+
 module "rds_db_instance" {
   source               = "./rds"
   db_subnet_group_name = "dev_proj_1_rds_subnet_group"
@@ -85,4 +85,4 @@ module "rds_db_instance" {
   mysql_username       = "dbuser"
   mysql_password       = "dbpassword"
   mysql_dbname         = "devprojdb"
-}*/
+}
